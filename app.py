1fabd43a45532f026a12e1c23078ac001103851a
invest-ment <130571971+invest-ment@users.noreply.github.com>
2023-04-16 17:58:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from prophet import Prophet
 import plotly.graph_objs as go
 # Set page title
 st.set_page_config(page_title='Crypto Prediction App',page_icon=":chart_with_upwards_trend:")
+st.markdown('[Home](https://cypherak007.github.io/financedecoder/index.html)')
 # Define function to get stock data
 @st.cache
 def load_data(ticker):
