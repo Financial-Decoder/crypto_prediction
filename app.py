2c2ac575c0f55ea6af468511453a0448768645d9
invest-ment <130571971+invest-ment@users.noreply.github.com>
2023-04-16 18:27:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import plotly.graph_objs as go
 st.set_page_config(page_title='Crypto Prediction App',page_icon=":chart_with_upwards_trend:")
 url = 'https://cypherak007.github.io/financedecoder/index.html'
 link_text = 'Home'
-st.markdown(f'<a target="_self" href="#" onclick="{js}">Home</a>', unsafe_allow_html=True)
+st.markdown(f'<a target="_self" href=url>Home</a>', unsafe_allow_html=True)
 # Define function to get stock data
 @st.cache
 def load_data(ticker):
